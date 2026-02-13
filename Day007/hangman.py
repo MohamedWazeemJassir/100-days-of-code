39c5569ea39d9dabb61b056def29048622d05ab9
Mohamed Wazeem Jassir <mohamedwazeemjassir@gmail.com>
2026-02-13 22:39:01 +0530

    Complete Day 7

--- Day007/hangman.py
+++ Day007/hangman.py
@@ -1,12 +1,52 @@
-from hangman_art import stages, logo
 import random
-word_list = ["aardvark", "baboon", "camel"]
+from hangman_words import word_list
+from hangman_art import logo, stages
+
+lives = 6
 
 print(logo)
+chosen_word = random.choice(word_list)
 
-# TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word. Then print it.
+placeholder = ""
+word_length = len(chosen_word)
+for position in range(word_length):
+    placeholder += "_"
+print("Word to guess: " + placeholder)
 
-# TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+game_over = False
+correct_letters = []
 
-# TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word. Print "Right" if it
-#  is, "Wrong" if it's not.
+while not game_over:
+
+    print(f"****************************<???>/{lives} LIVES LEFT****************************")
+    guess = input("Guess a letter: ").lower()
+
+    if guess in correct_letters :
+        print(f"You have already guessed {guess}.")
+    display = ""
+
+    for letter in chosen_word:
+        if letter == guess:
+            display += letter
+            correct_letters.append(guess)
+        elif letter in correct_letters:
+            display += letter
+        else:
+            display += "_"
+
+    print("Word to guess: " + display)
+
+    if guess not in chosen_word:
+        lives -= 1
+        print(f"You guessed {guess}, that's not in the word. You lose a life.")
+        if lives == 0:
+            game_over = True
+
+            print(f"***********************YOU LOSE**********************")
+            print(f"The word is {chosen_word}.")
+
+    if "_" not in display:
+        game_over = True
+        print("****************************YOU WIN****************************")
+
+    print(stages[lives])
